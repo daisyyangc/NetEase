f63ca71638df0206c86ab8e6fd795da55a91e156
WhiteDeng <dengxiaobai@dengxiaobaideMacBook-Pro.local>
2016-07-25 18:42:46 +0800

    项目初始化

---
+++ NetEase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetEase
+//
+//  Created by WhiteDeng on 16/7/21.
+//  Copyright © 2016年 pptv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
